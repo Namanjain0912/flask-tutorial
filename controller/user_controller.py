bd5598d8a9623f8d1eae85c495f33c69dde1cea6
Namanjain0912 <naman.jain0912@outlook.com>
2024-12-21 23:24:24 +0530

    endpoint name updated

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -1,7 +1,7 @@
 from app import app
 from model.user_model import user_model
 obj=user_model()
-@app.route("/user/getall")
+@app.route("/user/getfetch")
 def user_getall_controller():
     return obj.user_getall_model()  
     
